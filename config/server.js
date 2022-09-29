3d0e3dae0e6dd6530017753a7c12c7e088d0ba98
yumekiti <yumekiti120440@gmail.com>
2022-09-29 22:48:05 +0900

    `add` docker

--- config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '6d65b27e7ba207a44e06ae280b3db282'),
-    },
-  },
-});
